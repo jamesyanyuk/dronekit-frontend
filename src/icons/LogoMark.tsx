8214c2e526b8cb6585ed8c6e53a2c431a2d5a750
James Yanyuk <james@yanyuk.com>
2024-03-27 23:46:38 -0400

    v0

---
+++ src/icons/LogoMark.tsx
@@ -0,0 +1,23 @@
+export function LogoMark({ className }: { className?: string }) {
+  return (
+    <div className={className}>
+      <svg viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clipPath="url(#clip0_19_17)">
+          <path
+            d="M39.7801 381.55C27.0101 381.55 16.7801 376.97 9.10008 367.81C1.42008 358.65 -1.39992 347.68 0.630081 334.91L17.0101 230.8C19.0501 218.03 25.3801 207.06 36.0301 197.9C46.6701 188.74 58.3801 184.16 71.1501 184.16H193.02L208.29 86.4399H334.88L295.74 334.91C293.7 347.68 287.37 358.65 276.72 367.81C266.08 376.97 254.28 381.55 241.32 381.55H39.7801ZM145.27 220.81L125.56 344.91H167.48L187.19 220.81H145.27Z"
+            fill="currentColor"
+          />
+          <path
+            d="M327.91 275.06L384.29 184.17H511.99L451.19 279.95L476.18 381.56H348.2L327.91 275.07V275.06Z"
+            fill="currentColor"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_19_17">
+            <rect width="512" height="512" fill="currentColor" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,42 @@
+import { LogoMark } from "@/icons/logomark";
+import { TypeAnimation } from "react-type-animation";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col font-mono text-[13px] text-neutral-400">
+      <div className="m-auto">
+        <LogoMark className="size-10 flex-none" />
+        <p className="mt-1.5 w-full min-w-64">modular drone platform</p>
+        <code className="mt-6 block whitespace-pre-line rounded-lg bg-neutral-800 p-4 ring-1 ring-neutral-700">
+          <TypeAnimation
+            sequence={[
+              "# vertically integrated",
+              1000,
+              "# software & hardware",
+              1000,
+              "# plug & play",
+              1000,
+              "# made in the usa",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+        </code>
+      </div>
+
+      <footer className="bottom-0 inline-flex w-full flex-none p-6 text-neutral-400">
+        <span>(c) {new Date().getFullYear()} DroneKit</span>
+        <a
+          href="https://discord.gg/nkzPH2bnRB"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-auto no-underline hover:text-white"
+        >
+          discord
+        </a>
+      </footer>
+    </main>
+  );
+}
